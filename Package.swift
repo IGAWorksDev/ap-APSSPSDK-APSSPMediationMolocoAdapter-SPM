1c72416e40c1cd1c1c43dbb4df6087d060a083b1
HwanghoSong <odin.song@adpopcorn.com>
2026-09-01 10:16:44 +0900

    chore: .gitignore 추가 (.build, .swiftpm, Package.resolved 제외)

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "APSSPMediationMoloco",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(name: "APSSPMediationMoloco", targets: ["APSSPMediationMoloco"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/IGAWorksDev/ap-APSSPSDK-SPM.git", from: "3.4.0"),
+        .package(url: "https://github.com/moloco/moloco-sdk-ios-spm.git", exact: "4.10.0"),
+    ],
+    targets: [
+        .target(name: "APSSPMediationMoloco",
+                dependencies: [
+                    .product(name: "APSSPSDK", package: "ap-APSSPSDK-SPM"),
+                    .product(name: "MolocoSDK", package: "moloco-sdk-ios-spm"),
+                ],
+                path: "Sources/MediationMoloco"),
+    ]
+)
